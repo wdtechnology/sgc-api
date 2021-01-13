675d5b969f6ce3062510eec69c55ee1e7d5a72e5
Ailton Luiz <ailtonatn@gmail.com>
2021-01-13 10:59:42 +0100

    Iniciando projeto

---
+++ database/migrations/2021_01_12_122458_create_all_tables.php
@@ -0,0 +1,151 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAllTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function(Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('cpf')->unique();
+            $table->string('password');
+        });
+
+        Schema::create('units', function(Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->integer('id_owner');
+
+        });
+        Schema::create('unit_peoples', function(Blueprint $table){
+            $table->id();
+            $table->integer('id_unit');
+            $table->string('name');
+            $table->date('birth_date');
+
+        });
+        Schema::create('unit_vehicles', function(Blueprint $table){
+            $table->id();
+            $table->integer('id_unit');
+            $table->string('title');
+            $table->string('color');
+            $table->string('plate');
+
+
+        });
+        Schema::create('unit_pets', function(Blueprint $table){
+            $table->id();
+            $table->integer('id_unit');
+            $table->string('name');
+            $table->string('race');
+
+        });
+        Schema::create('walls', function(Blueprint $table){
+            $table->id();
+            $table->string('title');
+            $table->string('body');
+            $table->dateTime('date_created');
+
+        });
+        Schema::create('walls_likes', function(Blueprint $table){
+            $table->id();
+            $table->integer('id_wall');
+            $table->integer('id_user');
+
+        });
+        Schema::create('docs', function(Blueprint $table){
+            $table->id();
+            $table->string('title');
+            $table->string('file_url');
+
+        });
+        Schema::create('billets', function(Blueprint $table){
+            $table->id();
+            $table->integer('id_unit');
+            $table->string('title');
+            $table->string('file_url');
+
+        });
+        Schema::create('warnings', function(Blueprint $table){
+            $table->id();
+            $table->integer('id_unit');
+            $table->string('title');
+            $table->string('status')->default('IN_REVIEW'); //IN_REVIEW, RESOLVER
+            $table->date('date_create');
+            $table->text('photos');
+
+        });
+
+        Schema::create('found_and_lost', function(Blueprint $table){
+            $table->id();
+            $table->string('status')->default('LOST'); //LOST, RECOVERED
+            $table->string('photo');
+            $table->string('description');
+            $table->string('where');
+            $table->date('date_create');
+
+        });
+
+        Schema::create('areas', function(Blueprint $table){
+            $table->id();
+            $table->integer('allwoed')->default(1);
+            $table->string('title');
+            $table->string('cover');
+            $table->string('days');
+            $table->date('date_create');
+            $table->time('start_time');
+            $table->time('end_time');
+
+        });
+
+        Schema::create('area_disabled_days', function(Blueprint $table){
+            $table->id();
+            $table->integer('id_area');
+            $table->date('day');
+
+        });  Schema::create('reservations', function(Blueprint $table){
+            $table->id();
+            $table->integer('id_unit');
+            $table->integer('id_aarea');
+            $table->dateTime('reservation_date');
+
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('units');
+        Schema::dropIfExists('unit_peoples');
+        Schema::dropIfExists('unit_vehicles');
+        Schema::dropIfExists('unit_pets');
+        Schema::dropIfExists('walls');
+        Schema::dropIfExists('walls_likes');
+        Schema::dropIfExists('docs');
+        Schema::dropIfExists('billets');
+        Schema::dropIfExists('warnings');
+        Schema::dropIfExists('found_and_lost');
+        Schema::dropIfExists('areas');
+        Schema::dropIfExists('area_disabled_days');
+        Schema::dropIfExists('reservations');
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BilletController;
+use App\Http\Controllers\DocController;
+use App\Http\Controllers\FoundAndLostController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\UnitController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WallController;
+use App\Http\Controllers\WarningController;
+use GuzzleHttp\Middleware;
+
+Route::get('/ping', function(){
+    return['pong'=>true];
+});
+
+Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:api')->group(function(){
+    Route::post('auth/validate', [AuthController::class, 'validateToken']);
+    Route::post('auth/logout', [AuthController::class, 'logout']);
+
+    //Mural de avisos
+    Route::get('/walls', [WallController::class, 'getAll']);
+    Route::post('/wall/{id}/like', [WallController::class, 'like']);
+
+    //Documentos
+    Route::get('/docs',[DocController::class, 'getAll']);
+
+    // Livro de ocorrências
+    Route::get('/warnings', [WarningController::class, 'getMyWarnings']);
+    Route::post('/warning', [WarningController::class, 'setWarning']);
+    Route::post('/warning/file', [WarningController::class, 'addWarningFile']);
+
+    //Boletos
+    Route::get('/billets',[BilletController::class, 'getAll']);
+
+    //Achados e perdidos
+    Route::get('/foundAndLost',[FoundAndLostController::class,'getAll']);
+    Route::post('/foundAndLost',[FoundAndLostController::class, 'insert']);
+    Route::put('/foundAndLost/{id}',[FoundAndLostController::class, 'update']);
+
+    //Unidade
+    Route::get('/unit/{id}',[UnitController::class, 'getInfo']);
+    Route::post('/unit/{id}/addPerson', [UnitController::class, 'addPerson']);
+    Route::post('/unit/{id}/addVehicle', [UnitController::class, 'addVehcicle']);
+    Route::post('/unit/{id}/addPet', [UnitController::class, 'addPet']);
+    Route::post('/unit/{id}/removePerson', [UnitController::class, 'removePerson']);
+    Route::post('/unit/{id}/removeVehicle', [UnitController::class, 'removeVehcicle']);
+    Route::post('/unit/{id}/removePet', [UnitController::class, 'removePet']);
+
+    //Reservas
+    Route::get('/reservations',[ReservationController::class, 'getReservations']);
+    Route::post('reservation/{id}',[ReservationController::class, 'setReservation']);
+
+    Route::get('/reservation/{id}/disabledDates',[ReservationController::class, 'getDisabledDates']);
+    Route::get('/reservation/{id}/times', [ReservationController::class, 'getTimes']);
+
+    Route::get('/myReservations',[ReservationController::class, 'getMyReservations']);
+    Route::delete('/myReservation/{id}',[ReservationController::class, 'delMyReservation']);
+  
+
+});
+
+
